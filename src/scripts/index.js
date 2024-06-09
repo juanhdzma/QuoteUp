dcf3d72c6fcd05772a023212c1d7e5ae550aef16
Juan Hernandez <juanhdzma@hotmail.com>
2024-06-08 20:23:23 -0500

    Update button functionalities and styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,11 @@
 let quotes;
 
+const HALF_HEART = "m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3";
+const FULL_HEART = "m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z";
+
 document.getElementById('nextButton').addEventListener('click', displayRandomQuote);
 document.getElementById('copyButton').addEventListener('click', copyQuote);
+document.getElementById('likeButton').addEventListener('click', likeManager);
 
 document.addEventListener('DOMContentLoaded', () => {
   readFile().then(file => {
@@ -17,6 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function displayRandomQuote() {
   const quote = document.getElementById('quote');
   const author = document.getElementById('author');
+  const id = document.getElementById('quoteID');
+  const image = document.getElementById('likeImage');
   quote.style.opacity = 0;
   author.style.opacity = 0;
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -27,6 +33,14 @@ function displayRandomQuote() {
     setTimeout(() => {
       quote.innerHTML = '"' + randomQuote.quote + '"';
       author.innerHTML = "- " + randomQuote.author;
+      id.innerHTML = randomQuote.id;
+      quotesIsLiked(randomQuote.id, (isLiked) => {
+        if (isLiked) {
+          image.setAttribute("d", FULL_HEART);
+        } else {
+          image.setAttribute("d", HALF_HEART);
+        }
+      });
       quote.style.opacity = 1;
       author.style.opacity = 1;
     }, time);
@@ -55,4 +69,41 @@ function copyQuote() {
   setTimeout(() => {
     feedbackElement.style.opacity = 0;
   }, 800);
+}
+
+function likeManager() {
+  const image = document.getElementById('likeImage');
+  const id = parseInt(document.getElementById('quoteID').innerHTML);
+  if (image.getAttribute("d") == HALF_HEART) {
+    image.setAttribute("d", FULL_HEART);
+    addFavorite(id);
+  } else {
+    image.setAttribute("d", HALF_HEART);
+    removeFavorite(id);
+  }
+}
+
+function addFavorite(id) {
+  chrome.storage.sync.get(['favoriteQuotes'], (result) => {
+    let tmpQuotes = result.favoriteQuotes || [];
+    if (!tmpQuotes.includes(id)) {
+      tmpQuotes.push(id);
+      chrome.storage.sync.set({ favoriteQuotes: tmpQuotes });
+    }
+  });
+}
+
+function removeFavorite(id) {
+  chrome.storage.sync.get(['favoriteQuotes'], (result) => {
+    let tmpQuotes = result.favoriteQuotes || [];
+    tmpQuotes = tmpQuotes.filter((quote) => quote !== id);
+    chrome.storage.sync.set({ favoriteQuotes: tmpQuotes });
+  });
+}
+
+function quotesIsLiked(id, callback) {
+  chrome.storage.sync.get(['favoriteQuotes'], (result) => {
+    let tmpQuotes = result.favoriteQuotes || [];
+    callback(tmpQuotes.includes(id));
+  });
 }
